d7133e32230f5f53029c94c2c374dd0a5c36e800
MJ <themjtechnique@gmail.com>
2025-04-09 08:37:14 -0500

    final

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ function saludar() {
     var campoNombre = document.getElementById("nombre");
     var nombre = campoNombre.value;
     if (nombre !== "") {
-      alert("¡Hola, " + nombre + "! Gracias por estar aquí");
+      alert("¡Hola, " + nombre + "! Gracias por estar aquí 🐼🐻🐻‍❄️");
     } 
     
     else {
       alert("Por favor, escribe tu nombre.");
     }
   }
-  
